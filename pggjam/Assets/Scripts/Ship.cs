544cb5605f4db16c55cfceccc95829f843d7a83a
szymon112233 <szymmon123@gmail.com>
2017-11-25 03:24:48 +0100

    Added Glow

---
+++ pggjam/Assets/Scripts/Ship.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+    public GameObject otherShip;
+
+    private LineRenderer line;
+
+    public int lineSegments = 100;
+
+	// Use this for initialization
+	void Start () {
+        line = GetComponent<LineRenderer>();
+        line.positionCount = lineSegments;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 deltaVector = otherShip.transform.position - transform.position;
+        float segment = 0.0f;
+        for (int i = 0; i < lineSegments; i++)
+        {
+            segment = (float)i / (float)(lineSegments - 1);
+            line.SetPosition(i, transform.position + segment * deltaVector );
+        }
+        
+        
+    }
+}
