75856bb5aad82dfaaf65d5366acf07ed32a29ab3
dante <daniellewinski95@gmail.com>
2017-11-25 10:57:12 +0100

    Missing files.

---
+++ pggjam/Assets/Scripts/Model/SelectShipAction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model
+{
+	public class SelectShipAction : Action 
+	{
+		Vector2 position;
+
+		public SelectShipAction(Vector2 position)
+			: base(ActionType.SELECT)
+		{
+			this.position = position;
+		}
+
+		public override bool IsLegal(GameState state)
+		{
+			return state.Vessels[state.CurrentPlayer].Contains(position);
+		}
+
+		public override void ApplyAction(GameState state)
+		{
+			state.SelectedPosition = position;
+		}
+
+		public override void ApplyAction(Game game)
+		{
+			//game.Input = position;
+			game.AquireShip(position);
+		}
+
+		public override void Print()
+		{
+			Debug.Log("Selection " + position);
+		}
+
+		
+	}
+}
