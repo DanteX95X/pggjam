86b17a1332ef3ddcc10787469bb09cdf5c1eefe8
dante <daniellewinski95@gmail.com>
2017-11-27 21:47:47 +0100

    New menu scene. Some new icons.

---
+++ pggjam/Assets/Scripts/UI/ChoosePlayerButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoosePlayerButton : MonoBehaviour 
+{
+	[SerializeField]
+	Sprite[] sprite = null;
+
+	[SerializeField]
+	int index;
+
+	void Start () 
+	{
+		GetComponent<Image>().sprite = sprite[index];// = texture;
+	}
+
+	void Update () 
+	{
+		
+	}
+
+	public void ChangeSprite()
+	{
+		index = (index+1) % sprite.Length;
+		GetComponent<Image>().sprite = sprite[index];
+	}
+}
